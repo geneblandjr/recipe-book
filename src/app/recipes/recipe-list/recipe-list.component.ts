ad46294a6fe4056118fdb9a5035b9a6f8af8c3cd
Gene Bland <geneblandjr@gmail.com>
2017-01-05 14:37:54 -0500

    Commit through "detail" addition to recipe book application.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe';
 
@@ -9,10 +9,13 @@ import { Recipe } from '../recipe';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
+    @Output() recipeSelected = new EventEmitter<Recipe>();
     recipe = new Recipe('Dummy', 'Dummy Description', 'http://www.dcs.gla.ac.uk/~stephen/science_museum/dummy_head.jpg');
   constructor() { }
 
   ngOnInit() {
   }
-
+  onSelected(recipe: Recipe) {
+      this.recipeSelected.emit(recipe);
+  }
 }
